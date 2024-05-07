8a727e2e7cfe0108eee8757118ee660cdd3bbe14
kimjae-1 <kimjae1@g.skku.edu>
2024-05-07 02:58:53 +0000

    test - update-2

--- app/core/errors/error.py
+++ app/core/errors/error.py
@@ -4,7 +4,7 @@ ERROR_400_CLASS_CREATION_FAILED = "40002"
 ERROR_400_CLASS_NOTICE_CREATION_FAILED = "40003"
 ERROR_400_CLASS_NOTICE_UPDATE_FAILED = "40004"
 ERROR_400_CLASS_NOTICE_DELETE_FAILED = "40005"
-
+ERROR_400_USER_CREATION_FAILED = '40006'
 ERROR_401_INVALID_API_KEY = "40100"
 
 
@@ -62,6 +62,12 @@ class ClassNoticeDeleteFailed(BaseAPIException):
             message="Class Notice delete failed",
         )
 
+class UserCreationFailed(BaseAPIException):
+    def __init__(self):
+        super().__init__(
+            code=ERROR_400_USER_CREATION_FAILED,
+            message = 'USER_CREATION_FAILED'
+        )
 
 class InvalidAPIKey(BaseAuthException):
     def __init__(self):
